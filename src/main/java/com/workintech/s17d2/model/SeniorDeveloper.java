c0a547f38d261a571f3ac85aa729295cd85e3922
sinemsevimlikurt <sevimlikurtsinem@gmail.com>
2025-04-08 23:13:59 +0300

    to be worked on

---
+++ src/main/java/com/workintech/s17d2/model/SeniorDeveloper.java
@@ -0,0 +1,9 @@
+package com.workintech.s17d2.model;
+
+public class SeniorDeveloper extends Developer {
+
+    public SeniorDeveloper(int id, String name, double salary) {
+        super(id, name, salary, Experience.SENIOR);
+    }
+
+}
---
+++ src/main/java/com/workintech/s17d2/rest/DeveloperController.java
@@ -0,0 +1,75 @@
+package com.workintech.s17d2.rest;
+
+
+import com.workintech.s17d2.model.Developer;
+import com.workintech.s17d2.model.JuniorDeveloper;
+import com.workintech.s17d2.model.MidDeveloper;
+import com.workintech.s17d2.model.SeniorDeveloper;
+import jakarta.annotation.PostConstruct;
+import com.workintech.s17d2.tax.DeveloperTax;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/workintech/developers")
+public class DeveloperController {
+
+    public Map<Integer, Developer> developers;
+
+    private final DeveloperTax developerTax;
+
+    @Autowired
+    public DeveloperController( DeveloperTax developerTax) {
+        this.developerTax = developerTax;
+    }
+
+    @PostConstruct
+    public void init() {
+       developers = new HashMap<>();
+    }
+
+    @GetMapping
+    public List<Developer> getAllDevelopers() {
+        return new ArrayList<>(developers.values());
+    }
+
+    @GetMapping("/{id}")
+    public Developer getDeveloperById(@PathVariable int id) {
+        return developers.get(id);
+    }
+
+
+    @PostMapping
+    @ResponseStatus(org.springframework.http.HttpStatus.CREATED)
+    public void addDeveloper(@RequestBody Developer developerRequest) {
+        Developer developer;
+        switch (developerRequest.getExperience()) {
+            case JUNIOR:
+                developer = new JuniorDeveloper(developerRequest.getId(), developerRequest.getName(), developerRequest.getSalary() - developerRequest.getSalary() * developerTax.getSimpleTaxRate() / 100);
+                break;
+            case MID:
+                developer = new MidDeveloper(developerRequest.getId(), developerRequest.getName(), developerRequest.getSalary() - developerRequest.getSalary() * developerTax.getMiddleTaxRate() / 100);
+                break;
+            case SENIOR:
+                developer = new SeniorDeveloper(developerRequest.getId(), developerRequest.getName(), developerRequest.getSalary() - developerRequest.getSalary() * developerTax.getUpperTaxRate() / 100);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid experience level");
+        }
+        developers.put(developerRequest.getId(), developer);
+    }
+
+    @PutMapping("/{id}")
+    public Developer updateDeveloper(@PathVariable int id, @RequestBody Developer developerRequest) {
+        developers.put(id, developerRequest);
+        return developerRequest;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteDeveloper(@PathVariable int id) {
+        Developer removedDeveloper = developers.remove(id);
+        return removedDeveloper != null ? "Developer with ID " + id + " deleted" : "Developer not found";
+    }
+}
+
